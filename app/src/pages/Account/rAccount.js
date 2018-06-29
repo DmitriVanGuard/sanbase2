a1b0b0533b2510e89ab429e3630052b37372ced9
DmitriVanGuard <dimavanguard@gmail.com>
2018-06-29 23:03:52 +0300

    Simple Account component redux

--- app/src/pages/Account/rAccount.js
+++ app/src/pages/Account/rAccount.js
@@ -1,1 +1,30 @@
-import { changeEmailGQL, changeUsernameGQL } from './accountGQL'
+import React from 'react'
+import { connect } from 'react-redux'
+import { Form, Input, Message, Divider, Button } from 'semantic-ui-react'
+import AccountHeader from './AccountHeader'
+// import { changeEmailGQL, changeUsernameGQL } from './accountGQL'
+
+
+const rAccount = ({ user }) => {
+  return (
+    <div className='page account'>
+      <AccountHeader />
+      {!user.email &&
+        <Message
+          className='account-message'
+          warning
+          header='Email is not added yet!'
+          list={[
+            'For acces your dashboard from mobile device, you should add email address.'
+          ]}
+        />}
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  user: state.user.data,
+  loading: state.user.isLoading
+})
+
+export default connect(mapStateToProps)(rAccount)
